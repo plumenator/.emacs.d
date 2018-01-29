4cc77779f92491b876165509b443f10dbda104aa
Karthik Ravikanti <karthik.ravikanti@gmail.com>
2018-01-30 01:34:59 +0900

    Updated installed packages

--- init.el
+++ init.el
@@ -24,7 +24,7 @@
  '(package-enable-at-startup nil)
  '(package-selected-packages
    (quote
-    (magithub git-timemachine browse-at-remote use-package better-defaults which-key magit counsel swiper ivy)))
+    (smex intero haskell-mode lsp-rust rust-playground yasnippet company-racer ivy-hydra exec-path-from-shell cargo flycheck-rust racer rust-mode git-wip-timemachine magithub git-timemachine browse-at-remote use-package better-defaults which-key magit counsel swiper ivy)))
  '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
