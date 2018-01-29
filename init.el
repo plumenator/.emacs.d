5b5a3e99fdb7346fda95990a810dbd77e31987a4
Karthik Ravikanti <karthik.ravikanti@gmail.com>
2018-01-30 01:35:11 +0900

    User Command as Meta on Mac

--- init.el
+++ init.el
@@ -17,6 +17,7 @@
     ("d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" default)))
  '(inhibit-startup-screen t)
  '(menu-bar-mode nil)
+ '(ns-command-modifier (quote meta))
  '(package-archives
    (quote
     (("gnu" . "http://elpa.gnu.org/packages/")
