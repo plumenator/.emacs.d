84cd21bb6c68b8859760dc43fbf01d283d655a39
Karthik Ravikanti <karthik.ravikanti@gmail.com>
2018-05-25 12:00:22 +0800

    Add list of desirable packages

--- init.el
+++ init.el
@@ -28,7 +28,7 @@
  '(package-enable-at-startup nil)
  '(package-selected-packages
    (quote
-    (smex intero haskell-mode lsp-rust rust-playground yasnippet company-racer ivy-hydra exec-path-from-shell cargo flycheck-rust racer rust-mode git-wip-timemachine magithub git-timemachine browse-at-remote use-package better-defaults which-key magit counsel swiper ivy)))
+    (rotate editorconfig solarized-theme smex intero haskell-mode lsp-rust rust-playground yasnippet company-racer ivy-hydra exec-path-from-shell cargo flycheck-rust racer rust-mode git-wip-timemachine git-timemachine browse-at-remote use-package better-defaults which-key magit counsel swiper ivy)))
  '(rust-rustfmt-bin "rustfmt")
  '(tool-bar-mode nil))
 (custom-set-faces
