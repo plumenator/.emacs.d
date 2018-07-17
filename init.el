29b24334a3a3e39ba1b9ee360a834815d899669d
Karthik Ravikanti <karthik.ravikanti@gmail.com>
2018-07-17 13:29:46 +0800

    enable dired-find-alternate-file

--- init.el
+++ init.el
@@ -134,6 +134,7 @@
 ;; enable advanced functions
 (put 'narrow-to-region 'disabled nil)
 (put 'downcase-region 'disabled nil)
+(put 'dired-find-alternate-file 'disabled nil)
 
 ;; protobuf
 (require 'protobuf-mode)
