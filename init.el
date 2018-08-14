2077661f677ff1ca465977fa17c7fd90429c2f42
Karthik Ravikanti <karthik.ravikanti@gmail.com>
2018-08-14 15:31:37 +0800

    Install Nix mode

--- init.el
+++ init.el
@@ -27,7 +27,7 @@
  '(package-enable-at-startup nil)
  '(package-selected-packages
    (quote
-    (cider magit-todos smartparens rotate editorconfig solarized-theme smex intero haskell-mode lsp-rust rust-playground yasnippet company-racer ivy-hydra exec-path-from-shell cargo flycheck-rust racer rust-mode git-wip-timemachine git-timemachine browse-at-remote use-package better-defaults which-key magit counsel swiper ivy)))
+    (json-mode nix-mode cider magit-todos smartparens rotate editorconfig solarized-theme smex intero haskell-mode lsp-rust rust-playground yasnippet company-racer ivy-hydra exec-path-from-shell cargo flycheck-rust racer rust-mode git-wip-timemachine git-timemachine browse-at-remote use-package better-defaults which-key magit counsel swiper ivy)))
  '(rust-format-on-save t)
  '(rust-rustfmt-bin "rustfmt")
  '(tool-bar-mode nil)
@@ -144,6 +144,13 @@
 ;; protobuf
 (require 'protobuf-mode)
 (add-to-list 'auto-mode-alist '("\\.proto\\'" . protobuf-mode))
+
+;; nix
+(require 'nix-mode)
+(add-to-list 'auto-mode-alist '("\\.nix\\'" . nix-mode))
+(add-to-list 'auto-mode-alist '("\\.nix.in\\'" . nix-mode))
+(require 'nix-drv-mode)
+(add-to-list 'auto-mode-alist '("\\.drv\\'" . nix-drv-mode))
 
 (provide 'init)
 ;;; init.el ends here
