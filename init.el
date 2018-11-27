dbce4ae61fbc41053af0d649055ec020c4cf8a6b
Karthik Ravikanti <karthik.ravikanti@gmail.com>
2018-11-27 10:25:02 +0800

    Add joker based linting for clojure

--- init.el
+++ init.el
@@ -27,7 +27,7 @@
  '(package-enable-at-startup nil)
  '(package-selected-packages
    (quote
-    (feature-mode yaml-mode clang-format json-mode nix-mode cider magit-todos smartparens rotate editorconfig solarized-theme smex intero haskell-mode lsp-rust rust-playground yasnippet company-racer ivy-hydra exec-path-from-shell cargo flycheck-rust racer rust-mode git-wip-timemachine git-timemachine browse-at-remote use-package better-defaults which-key magit counsel swiper ivy)))
+    (flycheck-joker feature-mode yaml-mode clang-format json-mode nix-mode cider magit-todos smartparens rotate editorconfig solarized-theme smex intero haskell-mode lsp-rust rust-playground yasnippet company-racer ivy-hydra exec-path-from-shell cargo flycheck-rust racer rust-mode git-wip-timemachine git-timemachine browse-at-remote use-package better-defaults which-key magit counsel swiper ivy)))
  '(rust-format-on-save t)
  '(rust-rustfmt-bin "rustfmt")
  '(tool-bar-mode nil)
@@ -157,6 +157,9 @@
 ;; cucumber/gherkin feature files
 (require 'feature-mode)
 (add-to-list 'auto-mode-alist '("\.feature$" . feature-mode))
+
+;; clojure
+(require 'flycheck-joker)
 
 (provide 'init)
 ;;; init.el ends here
