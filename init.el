6f957542362131a9a50a1e824ea9a944e715a94a
Karthik Ravikanti <karthik.ravikanti@gmail.com>
2019-04-19 23:52:36 +0800

    Install flx for fuzzy matching

--- init.el
+++ init.el
@@ -29,7 +29,7 @@
  '(package-enable-at-startup nil)
  '(package-selected-packages
    (quote
-    (wgrep amx smart-mode-line-powerline-theme smart-mode-line corral ranger dired-ranger peep-dired idris-mode typescript-mode paredit flycheck-joker feature-mode yaml-mode clang-format json-mode nix-mode cider magit-todos smartparens rotate editorconfig solarized-theme intero haskell-mode lsp-rust rust-playground yasnippet company-racer ivy-hydra exec-path-from-shell cargo flycheck-rust racer rust-mode git-wip-timemachine git-timemachine browse-at-remote use-package better-defaults which-key magit counsel swiper ivy)))
+    (flx wgrep amx smart-mode-line-powerline-theme smart-mode-line corral ranger dired-ranger peep-dired idris-mode typescript-mode paredit flycheck-joker feature-mode yaml-mode clang-format json-mode nix-mode cider magit-todos smartparens rotate editorconfig solarized-theme intero haskell-mode lsp-rust rust-playground yasnippet company-racer ivy-hydra exec-path-from-shell cargo flycheck-rust racer rust-mode git-wip-timemachine git-timemachine browse-at-remote use-package better-defaults which-key magit counsel swiper ivy)))
  '(rust-format-on-save t)
  '(rust-rustfmt-bin "rustfmt")
  '(sml/mode-width
@@ -141,6 +141,8 @@
 (magit-todos-mode 1)
 
 (require 'wgrep)                        ; writable grep buffer
+
+(require 'flx)                          ; fuzzy matching
 
 ;; ivy-counsel-swiper
 (require 'ivy)
