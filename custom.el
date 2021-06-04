98acb6bdafadf7de24f0ce3ef88046eddfaacc00
Karthik Ravikanti <karthik.ravikanti@gmail.com>
2021-06-04 21:49:28 +0800

    Merge custom.el

--- custom.el
+++ custom.el
@@ -13,9 +13,12 @@
  '(magit-repository-directories '(("~" . 2)))
  '(ns-alternate-modifier 'none)
  '(ns-command-modifier 'meta)
+ '(package-archives
+   '(("gnu" . "http://elpa.gnu.org/packages/")
+     ("melpa" . "http://melpa.org/packages/")))
  '(package-enable-at-startup nil)
  '(package-selected-packages
-   '(lsp-ui cobalt dante go-direx go-eldoc go-mode rustic rust-auto-use flycheck-julia julia-mode julia-repl julia-shell jupyter forge avy flx wgrep amx smart-mode-line-powerline-theme smart-mode-line corral ranger dired-ranger peep-dired idris-mode typescript-mode paredit flycheck-joker feature-mode yaml-mode clang-format json-mode nix-mode cider magit-todos smartparens rotate editorconfig solarized-theme haskell-mode lsp-rust rust-playground yasnippet company-racer ivy-hydra exec-path-from-shell cargo flycheck-rust racer rust-mode git-wip-timemachine git-timemachine browse-at-remote use-package better-defaults which-key magit counsel swiper ivy))
+   '(amx avy better-defaults browse-at-remote cargo cider clang-format cobalt company-racer corral counsel dante dired-ranger editorconfig exec-path-from-shell feature-mode flx flycheck-joker flycheck-julia flycheck-rust forge git-timemachine git-wip-timemachine go-direx go-eldoc go-mode haskell-mode idris-mode ivy ivy-hydra json-mode julia-mode julia-repl julia-shell jupyter lsp-rust lsp-ui magit magit-todos nix-mode paredit peep-dired racer ranger rotate rust-auto-use rust-mode rust-playground rustic scalariform smart-mode-line smart-mode-line-powerline-theme smartparens solarized-theme swiper typescript-mode use-package wgrep which-key yaml-mode yasnippet))
  '(rust-format-on-save t)
  '(rust-rustfmt-bin "rustfmt")
  '(sml/mode-width (if (eq (powerline-current-separator) 'arrow) 'right 'full))
